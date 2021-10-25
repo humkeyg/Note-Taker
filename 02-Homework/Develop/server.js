0ebd381417dc9b9e5dec42eb49fa1ae668023617
humkeyg <87458845+humkeyg@users.noreply.github.com>
2021-10-24 19:24:37 -0700

    Deleted routes.js, Added to Server
    Deleted routes.js file and combined with server.js because it's not that long

--- 02-Homework/Develop/server.js
+++ 02-Homework/Develop/server.js
@@ -5,6 +5,24 @@ const PORT = 3001;
 
 app.use(express.json());
 
+const { application } = require("express");
+
+application.get('/api/notes', (req, res) => {
+    res.json(db.json);
+});
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/assets/notes.html'))
+);  
+
+app.get('*', (req, res) =>
+res.sendFile(path.join(__dirname, '../public/assets/index.html'))
+);
+
+app.get('/api/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '../db/db.json'))
+);
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );

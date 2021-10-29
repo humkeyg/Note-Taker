13f2276aeb2327f733d1e70811ff17d36400a93e
humkeyg <87458845+humkeyg@users.noreply.github.com>
2021-10-29 11:58:30 -0700

    added line 13, middleware

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
